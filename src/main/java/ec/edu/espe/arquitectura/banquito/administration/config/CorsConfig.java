69a4f51bf7310c60e2605e2e3266335162d3758a
Roberto Freire <79052020+bobbyrabia@users.noreply.github.com>
2023-08-28 18:23:08 -0500

    cors pa todo

--- src/main/java/ec/edu/espe/arquitectura/banquito/administration/config/CorsConfig.java
+++ src/main/java/ec/edu/espe/arquitectura/banquito/administration/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins("http://0.0.0.0")
                         .exposedHeaders("Content-Disposition")
                         .allowedMethods("*");
             }
