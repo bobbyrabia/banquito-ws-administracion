4be2e021b8ad2799260f09a04340547d82c36d0e
DayannaSilva <79052020+bobbyrabia@users.noreply.github.com>
2023-08-31 19:29:44 -0500

    Changes

--- src/main/java/ec/edu/espe/arquitectura/banquito/administration/config/Cors.java
+++ src/main/java/ec/edu/espe/arquitectura/banquito/administration/config/Cors.java
@@ -14,7 +14,6 @@ public class Cors {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:4200")
-
                         .exposedHeaders("Content-Disposition")
                         .allowedMethods("*");
             }
